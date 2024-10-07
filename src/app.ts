1256fc99b62a23a333a4f4594606882d3f243b10
jayedbinnazir <jayed.freelance@gmail.com>
2024-10-07 18:17:01 +0600

    setting up error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import CreateError, { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res, next: NextFunction) => {
+   const err = CreateError(401, "unauthorized user");
+   return next(err);
    res.send("auth-service");
+});
+
+//error handler
+
+// eslint-disable-next-line no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+   logger.error(err.message);
+   const statusCode = err.statusCode || 500;
+
+   res.status(statusCode).json({
+      errors: [
+         {
+            type: err.name,
+            msg: err.message,
+            location: "",
+            path: "",
+         },
+      ],
+   });
 });
 
 export default app;
